9cac8261433f8a50b5350cb80af478bdc07b9a5c
Dandridge Cole <dandridgecole@yahoo.com>
2019-06-20 17:43:14 -0400

    All tests passing

--- src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
+++ src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
@@ -14,26 +14,49 @@ public class ArrayUtility<E> {
     }
 
     public Integer countDuplicatesInMerge(E[] arrayToMerge, E valueToEvaluate){
-        return -1;
+        E[] newArray = mergeArrays(arrayToMerge);
+        return countDuplicates(valueToEvaluate, newArray);
+    }
+
+    private Integer countDuplicates(E valueToEvaluate, E[] newArray) {
+        Integer counter=0;
+        for(E element:newArray) if(element.equals(valueToEvaluate)) counter++;
+        return counter;
     }
 
     public E getMostCommonFromMerge(E[] arrayToMerge){
-        return null;
+        Integer max=0;
+        E value=null;
+        E[] newArray = mergeArrays(arrayToMerge);
+        for(E element:newArray) {
+            Integer thisCount=countDuplicates(element,newArray);
+            if(thisCount>max){
+                max=thisCount;
+                value=element;
+            }
+        }
+        return value;
     }
 
     public Integer getNumberOfOccurrences(E valueToEvaluate){
-        return -1;
+        return countDuplicates(valueToEvaluate,this.array);
     }
 
     public E[] removeValue(E valueToRemove){
-        return array;
+        Integer countRemove = getNumberOfOccurrences(valueToRemove);
+        Integer counter=0;
+        E[] newArray = Arrays.copyOf(this.array,this.array.length-countRemove);
+        for(E element:this.array)if(!element.equals(valueToRemove))newArray[counter++]=element;
+        return newArray;
     }
 
     private E[] mergeArrays(E[] arrayToMerge){
-            List<E> al = Arrays.asList(this.array);
-            al.addAll(Arrays.asList(arrayToMerge));
-            E[] newArray = arrayToMerge.clone();
-                    al.toArray(newArray);
+        E[] newArray = Arrays.copyOf(this.array,this.array.length+arrayToMerge.length);
+        for(int i =0;i<arrayToMerge.length;i++) newArray[i+this.array.length]=arrayToMerge[i];
+//        List<E> al = Arrays.asList(this.array);
+//        al.addAll(Arrays.asList(arrayToMerge));
+//        E[] newArray = Arrays.copyOf(this.array,al.size()+arrayToMerge.length);
+//        al.toArray(newArray);
         return newArray;
     }
 }
